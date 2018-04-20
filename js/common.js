96e18f53864978e152ccc41cddf7779c09a669cf
Jayesh Agrawal <jay.agrawal2011@live.com>
2018-04-21 01:03:20 +0530

    Added More Static Charts

---
+++ js/common.js
@@ -0,0 +1,91 @@
+function zoomInCharts(options) {
+    console.log("zoomInCharts called ...");
+    //TO DO options should contain lot more info to make dyanmic for each charts based on parameter
+    var commonData = [{
+        "name": "Year 1800",
+        "data": [107, 31, 635, 203],
+        "country": "Africa"
+    }, {
+        "name": "Year 1900",
+        "data": [133, 156, 947, 408],
+        "country": "America"
+    }, {
+        "name": "Year 2000",
+        "data": [814, 841, 3714, 727],
+        "country": "Asia"
+    }, {
+        "name": "Year 2016",
+        "data": [1216, 1001, 4436, 738],
+        "country": "Europe"
+    }];
+    var chart = c3.generate({
+        bindto: '#datasetComparsionPopupContent',
+        data: {
+            x: 'x',
+            columns: [
+                ['x', "Africa", "America", "Asia", "Europe"],
+                ['Year 1800', 107, 31, 635, 203],
+                ['Year 1900', 133, 156, 947, 408],
+                ['Year 2000', 814, 841, 3714, 727],
+                ['Year 2010', 1216, 1001, 4436, 738]
+            ],
+            //x:'country',
+            //json:commonData,
+            //json:_.pluck(commonData,"data") ,
+            type: 'bar',
+            labels: true
+                // keys: {
+                // //    tick:{ value: _.pluck(commonData,"country")}
+
+            // }
+        },
+        axis: {
+            x: {
+                type: 'category' // this needed to load string x value
+            }
+        },
+        bar: {
+            width: {
+                ratio: 0.5 // this makes bar width 50% of length between ticks
+            }
+            // or
+            //width: 100 // this makes bar width 100px
+        },
+        grid: {
+            x: {
+                show: true
+            },
+            y: {
+                show: true
+            }
+        }
+    });
+}
+$(function() {
+    console.log("DOM is ready");
+    $(".togglechart").click(function() {
+        console.log("togglechart BUTTON CLICKED.");
+
+
+        // Do Some calculation or some stuff for zomm in chart
+        // Clear old contents from popup
+        $('#datasetComparsionPopupContent').empty();
+        // Update requested charts in Zoom mode
+        // For Temporary purpose used setTimout function as popup opened after zoomInCharts function
+        setTimeout(function() {
+            zoomInCharts();
+        }, 0);
+
+        //show thw popup
+
+        $('#datasetComparsionPopup').show();
+    });
+    $(".js-closeDatasetComparsionPopup").click(function() {
+        console.log("**closeDatasetComparsionPopup POPUP closed");
+        // Hide pop up of zoom charts
+        $('#datasetComparsionPopup').hide();
+    });
+
+
+
+});
---
+++ js/googleanalyics.js
@@ -0,0 +1,8 @@
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('create', 'UA-91024451-2', 'auto');
+gtag('send', 'pageview');
+gtag('config', 'UA-91024451-2');
